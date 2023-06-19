e4fae6abbbdabb40c634f9f959eef06d662d1115
Dewa <vedran.devcic55@gmail.com>
2023-06-19 02:06:27 +0200

    Update main.go

--- main.go
+++ main.go
@@ -10,9 +10,16 @@ import (
 	"github.com/go-rod/rod"
 )
 
+const (
+	targetURL     = "https://registry.terraform.io/providers/hashicorp/template/latest/docs"
+	xpath         = "/html/body/div[3]/div[2]/div/div/article/div[1]/div/div"
+	deprecationMsg = "This provider is deprecated."
+	errorMsg      = "An error occurred while checking the page: %v"
+)
+
 func main() {
 	// Run the `terraform providers` command and capture the output
-	output, err := runTerraformCommand("providers")
+	output, err := runTerraformCommand()
 	if err != nil {
 		log.Fatalf("Failed to execute 'terraform providers' command: %v", err)
 	}
@@ -21,19 +28,22 @@ func main() {
 	text := string(output)
 
 	// Define the regular expression pattern
-	pattern := regexp.MustCompile(`provider\[registry\.terraform\.io/[^\]]+\]`)
+	regex := regexp.MustCompile(`provider\[registry\.terraform\.io/hashicorp/([^/\]]+)`)
 
-	// Find all matches in the text
-	matches := pattern.FindAllString(text, -1)
+    lines := strings.Split(text, "\n")
 
-	// Deduplicate the matches
-	uniqueProviders := make(map[string]bool)
-	for _, match := range matches {
-		provider := strings.TrimPrefix(match, "provider[registry.terraform.io/")
-		provider = strings.TrimSuffix(provider, "]")
-		uniqueProviders[provider] = true
-	}
+// Deduplicate the matches
+    uniqueProviders := make(map[string]bool)
 
+    for _, line := range lines {
+        if !strings.Contains(line, "module.") {
+            match := regex.FindStringSubmatch(line)
+            if len(match) > 1 {
+                trimmed := match[1]
+                uniqueProviders[trimmed] = true
+            }
+        }
+    }
 	// Check deprecation status for each provider
 	for provider := range uniqueProviders {
 		targetURL := fmt.Sprintf("https://registry.terraform.io/providers/hashicorp/%s/latest/docs", provider)
@@ -52,7 +62,7 @@ func main() {
 
 func runTerraformCommand(args ...string) ([]byte, error) {
 	// Specify the command to execute
-	command := exec.Command("terraform", args...)
+	command := exec.Command("terraform", "providers")
 
 	// Execute the command and capture the output
 	output, err := command.CombinedOutput()
@@ -76,15 +86,15 @@ func checkDeprecationStatus(targetURL string) (bool, error) {
 	// Wait for the page to load
 	page.MustWaitLoad()
 
-	// Find the deprecation message on the page
-	elem := page.MustElement("body")
+	// Evaluate the XPath expression to retrieve the deprecation message
+	elem := page.MustElementX(xpath)
 	deprecationText, err := elem.Text()
 	if err != nil {
-		return false, fmt.Errorf("failed to retrieve the deprecation message: %v", err)
+		log.Fatalf("Failed to retrieve the deprecation message: %v", err)
 	}
 
-	// Check if the deprecation message is present
-	isDeprecated := strings.Contains(deprecationText, "This provider is deprecated.")
+	// Check if the deprecation message indicates deprecation
+	isDeprecated := strings.Contains(deprecationText, deprecationMsg)
 
 	// Close the browser
 	browser.MustClose()
